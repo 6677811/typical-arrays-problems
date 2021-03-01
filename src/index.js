aa3024d0000e9b100991e385d56bb786067ad01c
dmitry_kalodzka <dmitry.kalodzka@certaintysoftware.com>
2021-03-01 22:31:37 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (...array) {
+    if (array[0] === undefined || array[0].length === 0) {
+        return 0;
+    }
+    const res = Math.min(...array[0]);
+    return res;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (...array) {
+    if (array[0] === undefined || array[0].length === 0) {
+        return 0;
+    }
+    return Math.max(...array[0]);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (...array) {
+    if (array[0] === undefined || array[0].length === 0) {
+        return 0;
+    }
+
+  const sum = array[0].reduce((a, b) => a + b, 0);
+  const avg = (sum / array[0].length) || 0;
+  return avg;
 }
